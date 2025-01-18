348dfdd38bf7dfb5eb5eb469b0045d34d282e995
mynk.chn <mynkchn.pu@gmail.com>
2025-01-18 13:36:20 +0530

    prepare for railway

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = 'django-insecure-31hvlcszgg8qv%(jq)am2b!ou4ajb2zzw_e*m$z@-g6ing2^kn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.railway.app' # https://saas.prod.railway.app
+]
+if DEBUG :
+    ALLOWED_HOSTS+=['127.0.0.1','localhost']
 
 
 # Application definition
--- src/visits/models.py
+++ src/visits/models.py
@@ -4,4 +4,6 @@ from django.db import models
 class PageVisit(models.Model) :
     path=models.TextField()
     timestamp=models.DateTimeField(auto_now_add=True)
+    def __str__(self) :
+        return self.timestamp
     pass
