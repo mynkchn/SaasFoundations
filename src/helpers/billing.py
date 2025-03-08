b99999f273b24c8876da23a88306baf0bef77240
mynk.chn <mynkchn.pu@gmail.com>
2025-03-08 12:15:07 +0530

    update

--- src/helpers/billing.py
+++ src/helpers/billing.py
@@ -1,7 +1,8 @@
 # Set your secret key. Remember to switch to your live secret key in production.
 # See your keys here: https://dashboard.stripe.com/apikeys
 import stripe
-stripe.api_key = "sk_test_tR3PYbcVNZZ796tH88S4VQ2u"
+from decouple import config
+stripe.api_key = config('STRIPE_API_KEY',cast=str,default=None)
 
 stripe.Customer.create(
   name="Jenny Rosen",
